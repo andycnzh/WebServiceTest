4e4840070d5654c95ebc542960c1e97367ac9a3c
andycnzh <andycnzh@outlook.com>
2017-10-09 14:24:57 +0800

    Add webservice test client and unit test

---
+++ GlobalWeatherTest.UnitTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GlobalWeatherTest.GlobalWeatherReference.Fakes;
+using Microsoft.QualityTools.Testing.Fakes;
+using GlobalWeatherTest.GlobalWeatherReference;
+
+namespace GlobalWeatherTest.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (ShimsContext.Create())
+            {
+                WebServiceHelper();
+
+                string endpoint = "GlobalWeatherSoap12";
+                GlobalWeatherSoapClient client = new GlobalWeatherSoapClient(endpoint);
+
+                string citiesList = client.GetCitiesByCountry("China");
+
+                Assert.AreEqual("Fake Cities", citiesList);
+            }
+        }
+
+        public void WebServiceHelper()
+        {
+            ShimGlobalWeatherSoapClient.AllInstances.GetCitiesByCountryString = (service, countryName) =>
+            {
+                return "Fake Cities";
+            };
+        }
+    }
+}
---
+++ GlobalWeatherTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using GlobalWeatherTest.GlobalWeatherReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalWeatherTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string endpoint = "GlobalWeatherSoap12";
+
+            GlobalWeatherSoapClient client = new GlobalWeatherSoapClient(endpoint);
+
+            string citiesList = client.GetCitiesByCountry("China");
+
+            Console.WriteLine(citiesList);
+
+            Console.ReadLine();
+        }
+    }
+}
